39452a0b4c3d8b3016b4a87fc2f681ca70686acb
David Born <42901656+davidBorn@users.noreply.github.com>
2020-12-29 20:15:09 -0700

    Add files via upload

---
+++ fonts-downloader.py
@@ -0,0 +1,44 @@
+import os
+import re
+import shutil
+import webbrowser
+
+siteFolder = input("Please enter folder name where your site files are located:")
+siteName = input("Please enter the filename of your stored website files:")
+fileType = input("Please enter the file (css, js, etc.):")
+fileName = input("Please enter what file to search for fonts:")
+fontPathStart = input("Please enter the start for each font link:")
+chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+path = "/Users/David/Documents/"+siteFolder+"/"+siteName+"_files/"+fileType+"/"+fileName
+
+linkArray=[]
+
+with open(path, 'r', encoding="utf-8") as cssFile:
+    for line in cssFile:
+        eotPattern = "[\/()-_%&0-9a-zA-Z \.]+\.eot"
+        svgPattern = "[\/()-_%&0-9a-zA-Z \.]+\.svg"
+        woffPattern = "[\/()-_%&0-9a-zA-Z \.]+\.woff"
+        woff2Pattern = "[\/()-_%&0-9a-zA-Z \.]+\.woff2"
+        if re.findall("url"+ eotPattern,line):
+            fontPath = re.findall("url"+eotPattern,line)
+            linkArray.append(fontPath[0])
+        if re.findall("url"+ svgPattern,line):
+            fontPath = re.findall("url"+svgPattern,line)
+            linkArray.append(fontPath[0])
+        if re.findall("url"+ woffPattern,line):
+            fontPath = re.findall("url"+woffPattern,line)
+            linkArray.append(fontPath[0])
+        if re.findall("url"+ woff2Pattern,line):
+            fontPath = re.findall("url"+woff2Pattern,line)
+            linkArray.append(fontPath[0])
+
+for i in range(len(linkArray)):
+    fontPathEnd = linkArray[i]
+    linkArray[i] = fontPathEnd.replace('url(',fontPathStart)
+    webbrowser.get(chrome_path).open(linkArray[i])
+
+print(linkArray)       
+
+
+#webbrowser.get(chrome_path).open('youtube.com');
+#webbrowser.get(chrome_path).open('https://www.boeing.com/assets/fonts/bcf54343-d033-41ee-bbd7-2b77df3fe7ba.woff');
